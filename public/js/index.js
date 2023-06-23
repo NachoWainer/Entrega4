6931e775b8edef2954aad1701df61bec3dd6030d
Ignacio Wainer <nachowainer@gmail.com>
2023-06-22 23:15:00 -0300

    entrega 4 v1

--- public/js/index.js
+++ public/js/index.js
@@ -5,24 +5,60 @@ socket.on("productos", (productos) => {
     if (productElement !== null){
     productElement.innerHTML = "";
     
-    
     productos.forEach((producto) => {
-      const li = document.createElement("li");
-      li.textContent = "id:"+`${producto.id}`+ "  "+ "title:"+`${producto.title}`+ "  "+`price:$ ${producto.price}` +"  " + `description:${producto.description}` +"   " + `code:${producto.code}`
-      +"   " + `status:${producto.status}`+"   " + `stock${producto.stock}`+"   " + `category:${producto.category}`;
-      productElement.appendChild(li);
+    const content = document.createElement("div");
+    content.innerHTML = `
+    <div class="producto ">
+
+    <p>ID:${producto.id}</p>
+    <p>category: ${producto.category}</p>
+    <div class = "productInfo">
+    <div class="productNameStock">
+    <p class="productName">title: ${producto.title}</p>
+    <p class="productName"> description: ${producto.description}</p>
+    <p class="productName">code: ${producto.code}</p>
+
+    <p>(stock: ${producto.stock})</p>
+    </div>
+    <p class="productName">price: $  ${producto.price}</p>
+    <p>status: ${producto.status}</p>
+    </div>
+    </div>
+  `;
+      
+      productElement.appendChild(content);
     });}
+    
   });
 
   socket.on("realTimeProducts", (realTimeProducts) => {
     const RTproductElement = document.getElementById("realTimeProduct");
-    if (RTproductElement !== null){RTproductElement.innerHTML = "";
     
-  
-    realTimeProducts.forEach((productoTR) => {
-      const li = document.createElement("li");
-      li.textContent = "id:"+`${productoTR.id}`+ "  "+ "title:"+`${productoTR.title}`+ "  "+`price:$ ${productoTR.price}` +"  " + `description:${productoTR.description}` +"   " + `code:${productoTR.code}`
-      +"   " + `status:${productoTR.status}`+"   " + `stock${productoTR.stock}`+"   " + `category:${productoTR.category}`;
-      RTproductElement.appendChild(li);
+
+    if (RTproductElement !== null){
+    RTproductElement.innerHTML = "";
+    
+    realTimeProducts.forEach((producto) => {
+    const content = document.createElement("div");
+    content.innerHTML = `
+    <div class="producto ">
+
+    <p>ID:${producto.id}</p>
+    <p>category: ${producto.category}</p>
+    <div class = "productInfo">
+    <div class="productNameStock">
+    <p class="productName">title: ${producto.title}</p>
+    <p class="productName"> description: ${producto.description}</p>
+    <p class="productName">code: ${producto.code}</p>
+
+    <p>(stock: ${producto.stock})</p>
+    </div>
+    <p class="productName">price: $  ${producto.price}</p>
+    <p>status: ${producto.status}</p>
+    </div>
+    </div>
+  `;
+      
+      RTproductElement.appendChild(content);
     });}
-  });
+    ;});
